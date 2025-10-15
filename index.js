38037d1fa9c687d2fa9eeee4c317cb093ffa2c92
sekarasriii <sekareeesh@gmail.com>
2025-10-15 12:00:51 +0700

    add koneksi ke database Mysql

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  port: '3309',
+  password: '@21baplanGGG',
+  database: 'mahasiswa'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('Error connecting to Mysql: ' + err.stack);
+    return;
+  }
+  console.log('Connected to Mysql successfully');
+});
+
